68709a609a1434223b629dd060486082f6a1de69
ZamanovRenat <zamanov.renat@mail.ru>
2021-12-01 17:01:30 +0300

    Add project files.

---
+++ HotelApplication/Controllers/HotelController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using HotelApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace HotelApplication.Controllers
+{
+    public class HotelController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        private static readonly List<Client> _clients = new()
+        {
+            new Client() { Id = 1, FirstName = "Ivan", LastName = "Ivanov", Patronymic = "Ivanovich", PassportData = "Russian", Notes = "No Smoking" },
+            new Client() { Id = 2, FirstName = "Ivan", LastName = "Ivanov", Patronymic = "Ivanovich", PassportData = "Russian", Notes = "No Smoking" },
+            new Client() { Id = 3, FirstName = "Ivan", LastName = "Ivanov", Patronymic = "Ivanovich", PassportData = "Russian", Notes = "No Smoking" },
+        };
+
+        public HotelController(IConfiguration configuration) => _configuration = configuration;
+
+        public IActionResult Index()
+        {
+            return View(_clients);
+        }
+    }
+}
---
+++ HotelApplication/Models/Checkin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HotelApplication.Models
+{
+    public class Checkin
+    {
+        public int Id { get; set; }
+        public Client Client { get; set; }
+        public Room Room { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public string Notes { get; set; }
+    }
+}
