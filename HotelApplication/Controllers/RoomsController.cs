ca9cae1a1141554d6fb3f12834a6811a318eec0f
ZamanovRenat <zamanov.renat@mail.ru>
2021-12-02 16:06:12 +0300

    Добавление возможности просмотра информации, удаления редактирования комнат на вьюшку

--- HotelApplication/Controllers/RoomsController.cs
+++ HotelApplication/Controllers/RoomsController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using HotelApplication.Models;
 using HotelApplication.Services.Interfaces;
 using HotelApplication.ViewModels;
